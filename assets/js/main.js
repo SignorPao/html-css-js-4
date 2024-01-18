2348b04c6cbc40342a13fe03524e3cb2d59c65e0
Pavel Belousov <signorpao@gmail.com>
2024-01-18 18:57:42 +0300

    home

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,8 +18,24 @@ if (navClose) {
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SHOW SCROLL UP ===============*/
 
